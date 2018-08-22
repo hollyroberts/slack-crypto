d8cf0a4b5f27afea97a3d764c7b49055ed518718
blackenedgem <Chrisroberts97@btinternet.com>
2018-08-22 18:11:03 +0100

    Add ability to calculate threshold with offset

--- notifier.py
+++ notifier.py
@@ -6,15 +6,31 @@ from coinbase import Coinbase
 # Configurable Constants
 PRIMARY_CURRENCY = "BTC"
 SECONDARY_CURRENCY = "USD"
+EMA_THRESHOLD_PERCENT = 2.5
 EMA_NUM_HOURS = 12
+HOURS_BETWEEN_POSTS = 6
 
 # 'Hard' Constants
 API_URL = "https://api.pro.coinbase.com"
 CURRENCY_PAIR = f"{PRIMARY_CURRENCY}-{SECONDARY_CURRENCY}"
 
+# region Functions
+def outside_threshold(price_data: list, offset: int):
+    if offset > 0:
+        price_data = price_data[offset:]
+
+    cur_price = price_data[0]
+    ema = Stats.ema(price_data[::-1], EMA_NUM_HOURS)
+
+    diff = abs(cur_price - ema)
+    percent_diff = diff / ema
+    percent_diff *= 100
+    return percent_diff > EMA_THRESHOLD_PERCENT
+# endregion
+
 # Time param info
 time_now = datetime.now()
-time_start = time_now - timedelta(hours=12)
+time_start = time_now - timedelta(hours=12)  # Doesn't matter because CB will return 300 results
 params = {
     "start": time_start.isoformat(),
     "end": time_now.isoformat(),
@@ -25,7 +41,6 @@ params = {
 historical_data = requests.get(f"{API_URL}/products/{CURRENCY_PAIR}/candles", params=params).text
 cb = Coinbase(historical_data)
 prices = cb.price_list()
-ema = Stats.ema(prices[::-1], EMA_NUM_HOURS)
 
-print(f"{EMA_NUM_HOURS} Hour EMA: {ema}")
-print(f"Current price: {cb.latest_price()}")
+for i in range(24):
+    outside_threshold(prices, i)
