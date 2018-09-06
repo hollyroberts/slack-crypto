5d8de9e8b4007a7fa5ebd704c4a31d15880147c5
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-06 22:27:05 +0100

    Remove unused history params

--- history.py
+++ history.py
@@ -15,16 +15,11 @@ class History:
 
                 self.price = last_post['price']
                 self.rising = last_post['rising']
-                self.rising_str = "rising" if self.rising else "falling"
-
-                self.loaded = True
         except IOError:
             print("Couldn't load file, using default values")
             self.ema_reset = True
             self.price = None
             self.rising = True
-            self.rising_str = "ERR"
-            self.loaded = False
 
     def save(self):
         print(f"Updating {self.file_name}")
