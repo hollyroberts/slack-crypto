ecece3cddc410955a29e91ce535cd22f7665d6f4
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-09 13:14:21 +0100

    Add reset percentage as argparse param

--- notifier.py
+++ notifier.py
@@ -20,8 +20,10 @@ parser.add_argument("--ema", "-e", default=26, type=int,
                     help="Number of hours for EMA calculation")
 parser.add_argument("-i", "--interval", default=60, type=int, choices=[5, 15, 60],
                     help="Resolution of data to fetch from coinbase (minutes)")
-parser.add_argument("--threshold", "-t", default=2.5, type=float,
-                    help="Amount current price needs to be above EMA")
+parser.add_argument("--threshold-ema", "-te", default=2.5, type=float,
+                    help="Amount current price needs to be above/below the EMA")
+parser.add_argument("--threshold-reset", "-tr", default=1.25, type=float,
+                    help="Amount EMA price needs to be within last post price to reset")
 parser.add_argument("--json-name", "-j",
                     help="Name of the JSON file to store with extension (use this if you're running the script multiple"
                          "times with different parameters)")
@@ -30,8 +32,9 @@ args = parser.parse_args()
 
 # region Constants
 # Configurable Constants
-EMA_THRESHOLD_PERCENT = args.threshold
+EMA_THRESHOLD_PERCENT = args.threshold_ema
 EMA_NUM_HOURS = args.ema
+EMA_RESET_PERCENT = args.threshold_reset
 
 BOT_NAME = args.name
 SLACK_CHANNEL = args.channel
@@ -58,7 +61,7 @@ history = History(DATA_FILE)
 # Get stats from coinbase data
 # If change isn't large enough, then update history and exit
 stats = HourData(prices, EMA_NUM_HOURS)
-if Analysis.ema_checks(stats, history, EMA_THRESHOLD_PERCENT, 0.5):
+if Analysis.ema_checks(stats, history, EMA_THRESHOLD_PERCENT, EMA_RESET_PERCENT):
     sys.exit(1)
 
 if not Analysis.should_post(history, stats, prices, EMA_THRESHOLD_PERCENT):
