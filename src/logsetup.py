8f3b54a4f2222a0d2eec57c7fa011ce7329b51e6
blackenedgem <Chrisroberts97@btinternet.com>
2018-09-14 23:38:48 +0200

    Handle exceptions with logging

--- src/logsetup.py
+++ src/logsetup.py
@@ -7,12 +7,14 @@ import os
 class LogSetup:
     @staticmethod
     def setup(stdout: bool, file: bool, location: str):
+        # Basic setup
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
 
         log_formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
         log_formatter.default_msec_format = '%s.%03d'
 
+        # Setup standard output and file output logging
         if stdout:
             console_handler = logging.StreamHandler(sys.stdout)
             console_handler.setFormatter(log_formatter)
@@ -26,4 +28,12 @@ class LogSetup:
             file_handler.setFormatter(log_formatter)
             logger.addHandler(file_handler)
 
+        # Override default handling of uncaught exceptions (program crash)
+        sys.excepthook = LogSetup.handle_top_exception
+
         logger.info("Logging initialised")
+
+    # noinspection PyUnusedLocal
+    @staticmethod
+    def handle_top_exception(exctype, value, traceback):
+        logging.exception("An unhandled exception occurred", exc_info=(exctype, value, traceback))
